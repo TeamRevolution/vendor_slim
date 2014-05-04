d21f422900b580a9297c831b277bd9905deb82f3
neophite31 <hard.d.25@gmail.com>
2014-05-04 18:56:15 +0200

    Añadido Viper4 y actualizado PowerBattery
    Change-Id: Ie5df1ff86aeb4d6beb5f1270e6ff1eacbaedbe2e

--- config/common.mk
+++ config/common.mk
@@ -58,6 +58,7 @@ PRODUCT_COPY_FILES += \
     vendor/slim/prebuilt/common/app/RevoBackup.apk:system/app/RevoBackup.apk \
     vendor/slim/prebuilt/common/app/RevoWifi.apk:system/app/RevoWifi.apk \
     vendor/slim/prebuilt/common/app/PowerBattery.apk:system/priv-app/PowerBattery.apk
+    vendor/slim/prebuilt/common/app/Viper4.apk:system/app/Viper4.apk
 
 # Embed SuperUser
 SUPERUSER_EMBEDDED := true
